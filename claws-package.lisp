669dbe797994ab01636781b955720d345eb159e5
Pascal Costanza <pascal.costanza@intel.com>
2014-10-09 16:13:17 +0200

    Ported to SBCL.

--- claws-package.lisp
+++ claws-package.lisp
@@ -1,6 +1,7 @@
 (in-package :cl-user)
 
 (defpackage #:claws
+  (:use #:cl)
   (:export #:fork #:spawn #:sync #:parlet #:seqlet #:reset-workers))
 
    
--- claws.lisp
+++ claws.lisp
@@ -1,9 +1,72 @@
 (in-package :claws)
 
+(declaim (inline list-all-processes))
+
+(defun list-all-processes ()
+  #+lispworks (mp:list-all-processes)
+  #+sbcl (sb-thread:list-all-threads))
+
+(declaim (inline process-run))
+
+(defun process-run (name function &rest arguments)
+  (declare (dynamic-extent arguments))
+  #+lispworks (apply 'mp:process-run-function name '() function arguments)
+  #+sbcl (sb-thread:make-thread function :name name :arguments (copy-list arguments)))
+
+(declaim (inline process-name))
+
+(defun process-name (process)
+  #+lispworks (mp:process-name process)
+  #+sbcl (sb-thread:thread-name process))
+
 (defun ensure-process-killed (process)
+  #+lispworks
   (when (mp:process-alive-p process)
     (mp:process-kill process)
-    (mp:process-wait "process kill" (complement #'mp:process-alive-p) process)))
+    (mp:process-wait "process kill" (complement #'mp:process-alive-p) process))
+  #+sbcl
+  (when (sb-thread:thread-alive-p process)
+    (sb-thread:terminate-thread process)
+    (sb-thread:join-thread process :default nil)))
+
+(deftype lock ()
+  #+lispworks 'mp:lock
+  #+sbcl 'sb-thread:mutex)
+
+(declaim (inline make-lock))
+
+(defun make-lock (name)
+  #+lispworks (mp:make-lock :name name :safep nil)
+  #+sbcl (sb-thread:make-mutex :name name))
+
+(defmacro with-lock ((lock &optional (whostate nil whostatep) (timeout nil timeoutp)) &body body)
+  (declare (ignorable whostate))
+  #+lispworks
+  (cond (timeoutp  `(mp:with-lock (,lock ,whostate ,timeout) ,@body))
+        (whostatep `(mp:with-lock (,lock ,whostate) ,@body))
+        (t         `(mp:with-lock (,lock) ,@body)))
+  #+sbcl
+  (if timeoutp
+    (let ((timeoutv (copy-symbol 'timeout)))
+      `(let ((,timeoutv ,timeout))
+         (sb-thread:with-recursive-lock (,lock :wait-p (> ,timeoutv 0) :timeout ,timeoutv) ,@body)))
+    `(sb-thread:with-recursive-lock (,lock) ,@body)))
+
+(deftype atomic-int ()
+  #+lispworks 'fixnum
+  #+sbcl 'sb-ext:word)
+
+(defmacro atomic-incf (place)
+  #+lispworks
+  `(sys:atomic-fixnum-incf ,place)
+  #+sbcl
+  `(sb-ext:atomic-incf ,place))
+
+(defmacro defglobal (var &optional (value nil valuep))
+  #+lispworks (if valuep
+                `(hcl:defglobal-variable ,var ,value)
+                `(hcl:defglobal-variable ,var))
+  #+sbcl `(sb-ext:defglobal ,var ,value))
 
 (defmacro fetch/clear (place &optional default)
   (let ((temp (copy-symbol 'temp)))
@@ -14,13 +77,18 @@
 (defconstant +initial-thunk-vector-size+ #.(expt 2 12))
 
 (deftype thunk () '(function ()))
-(deftype thunk-vector () '(array thunk (*)))
+(deftype thunk-vector ()
+  #+lispworks '(array thunk (*))
+  #+sbcl 'simple-vector)
 
 (defun invalid-thunk ()
   (error "invalid function call"))
 
 (defun make-thunk-vector (size)
-  (make-array size :element-type 'thunk :initial-element #'invalid-thunk :single-thread t))
+  (make-array size 
+              #+lispworks :element-type #+lispworks 'thunk
+              #+lispworks :single-thread #+lispworks t
+              :initial-element #'invalid-thunk))
 
 (declaim (ftype (function (thunk-vector integer) thunk) cvref cvref/clear)
          (ftype (function (thunk thunk-vector integer) thunk) (setf cvref))
@@ -42,7 +110,7 @@
   (declare (thunk-vector vector) (integer index) #.*optimization*)
   (let ((mindex (nth-value 1 (truncate index (length vector)))))
     (declare (fixnum mindex))
-    (setf (svref vector mindex) new-value)))
+    (setf (aref vector mindex) new-value)))
 
 (declaim (ftype (function (thunk-vector integer integer) thunk-vector) grow))
 
@@ -63,25 +131,10 @@
               (fetch/clear (aref old-vector old-index)))
         finally (return new-vector)))
 
-#|
-(defun test-grow ()
-  (let* ((size (1+ (random 1000)))
-         (stop (if (= size 1) 1
-                 (1+ (random (1- size)))))
-         (start (random stop))
-         (vector (make-thunk-vector size)))
-    (loop for i from start below stop do
-          (let ((i i)) (setf (cvref vector i) (lambda () i))))
-    (let ((copy (copy-seq vector))
-          (new-vector (grow vector start stop)))
-      (assert (loop for i from start below stop
-                    always (eq (cvref copy i) (cvref new-vector i)))))))
-|#
-
 (defstruct (q (:constructor makeq
                (&optional (vector-size +initial-thunk-vector-size+) &aux
                           (active-vector (make-thunk-vector vector-size)))))
-  (lock (mp:make-lock :name "q-lock" :safep nil) :type mp:lock :read-only t)
+  (lock (make-lock "q-lock") :type lock :read-only t)
   (top 0 :type integer)
   (bottom 0 :type integer)
   (active-vector #() :type thunk-vector))
@@ -94,20 +147,21 @@
             (q-bottom q)
             (length (q-active-vector q)))))
 
-(defglobal-variable *main-q*)
-(defglobal-variable *helper-processes*)
+(defglobal *main-q*)
+(defglobal *helper-processes*)
 
 (declaim (ftype (function (q thunk)) push-bottom))
 
 (defun push-bottom (q thunk)
   (declare (q q) (thunk thunk) #.*optimization*)
-  (mp:with-lock ((q-lock q) "push-bottom")
+  (with-lock ((q-lock q) "push-bottom")
     (let* ((top (q-top q))
            (bottom (q-bottom q))
            (vector (q-active-vector q))
            (size (- bottom top)))
       (declare (integer top bottom) (thunk-vector vector) (fixnum size))
-      (cond ((and (zerop size) (eq q *main-q*))
+      (cond #+lispworks
+            ((and (zerop size) (eq q *main-q*))
              (mapc 'mp:process-poke *helper-processes*))
             ((= size (length vector))
              (setf vector (grow vector top bottom))
@@ -119,7 +173,7 @@
 
 (defun pop-bottom (q)
   (declare (q q) #.*optimization*)
-  (mp:with-lock ((q-lock q) "pop-bottom")
+  (with-lock ((q-lock q) "pop-bottom")
     (let* ((top (q-top q))
            (bottom (q-bottom q))
            (vector (q-active-vector q))
@@ -135,7 +189,7 @@
 
 (defun steal (q)
   (declare (q q) #.*optimization*)
-  (mp:with-lock ((q-lock q) "steal" 0)
+  (with-lock ((q-lock q) "steal" 0)
     (let* ((top (q-top q))
            (bottom (q-bottom q))
            (vector (q-active-vector q))
@@ -146,7 +200,7 @@
         (cvref/clear vector top)))))
 
 
-(defglobal-variable *all-queues*)
+(defglobal *all-queues*)
 
 (defvar *current-q*)
 (defvar *randomq-state* (make-random-state t))
@@ -157,8 +211,8 @@
 (declaim (inline make-join-context))
 
 (defstruct (join-context (:constructor make-join-context ()))
-  (started-tasks 0 :type fixnum)
-  (stopped-tasks 0 :type fixnum))
+  (started-tasks 0 :type atomic-int)
+  (stopped-tasks 0 :type atomic-int))
 
 (defvar *join-context*)
 
@@ -172,8 +226,7 @@
     (incf (join-context-started-tasks join-context))
     (push-bottom *current-q* (lambda ()
                                (unwind-protect (funcall thunk)
-                                 (sys:atomic-fixnum-incf
-                                  (join-context-stopped-tasks join-context)))))))
+                                 (atomic-incf (join-context-stopped-tasks join-context)))))))
 
 (defmacro spawn ((&rest vars) &body body)
   `(flet ((spawn-function ()
@@ -190,8 +243,8 @@
   (loop with join-context of-type join-context = *join-context*
         with current-q of-type q = *current-q*
         with thunk = t
-        until (= (the fixnum (join-context-started-tasks join-context))
-                 (the fixnum (join-context-stopped-tasks join-context)))
+        until (= (the atomic-int (join-context-started-tasks join-context))
+                 (the atomic-int (join-context-stopped-tasks join-context)))
         when (setq thunk (if thunk 
                            (pop-bottom current-q)
                            (steal (randomq))))
@@ -214,32 +267,34 @@
          (seqlet ,(cdr bindings) ,@body))
     `(locally ,@body)))
 
-(defglobal-parameter *worker-thread-name* "claws-worker-thread")
+(defglobal *worker-thread-name* "claws-worker-thread")
 
 (defun poll-work (current-q)
   (let ((*current-q* current-q)
         (*randomq-state* (make-random-state t))
-        (pause 1.0E-9))
+        #+lispworks (pause 1.0E-9))
     (flet ((make-steal-attempts ()
-             (loop repeat (length *all-queues*)
+             (loop #+lispworks repeat #+lispworks (length *all-queues*)
                    when (steal (randomq)) return it)))
       (tagbody
-       :pop-local (when-let (thunk (pop-bottom current-q))
-                    (funcall (the thunk thunk))
-                    (go :pop-local))
-       :steal     (when-let (thunk (make-steal-attempts))
-                    (funcall (the thunk thunk))
-                    (go :pop-local))
-       :pause     (if (mp:current-process-pause pause)
-                    (setq pause 1.0E-9)
-                    (setq pause (* 2.0E0 pause)))
+       :pop-local  (let ((thunk (pop-bottom current-q)))
+                     (when thunk
+                       (funcall (the thunk thunk))
+                       (go :pop-local)))
+       :steal      (let ((thunk (make-steal-attempts)))
+                     (when thunk
+                       (funcall (the thunk thunk))
+                       (go :pop-local)))
+       #+lispworks (if (mp:current-process-pause pause)
+                     (setq pause 1.0E-9)
+                     (setq pause (* 2.0E0 pause)))
        (go :steal)))))
 
 (defun reset-workers (nof-processes)
-  (loop for process in (mp:list-all-processes)
-        when (string= (mp:process-name process) *worker-thread-name*)
+  (loop for process in (list-all-processes)
+        when (string= (process-name process) *worker-thread-name*)
         do (ensure-process-killed process))
-  (loop with all-queues = (make-array nof-processes :single-thread t)
+  (loop with all-queues = (make-array nof-processes #+lispworks :single-thread #+lispworks t)
         for i below nof-processes do (setf (svref all-queues i) (makeq))
         finally
         (setq *all-queues* all-queues)
@@ -248,7 +303,7 @@
         (setq *main-q* *current-q*)
         (setq *helper-processes*
               (loop for i from 1 below nof-processes
-                    collect (mp:process-run-function *worker-thread-name* '() 'poll-work (svref *all-queues* i)))))
+                    collect (process-run *worker-thread-name* 'poll-work (svref *all-queues* i)))))
   :ready)
 
 (reset-workers 1)
